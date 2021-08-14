daf35928a33002da251cfe132271dbb7bf5ca4ae
Dinesh Pabbi <dineshpabbi10@gmail.com>
2021-08-13 21:22:20 -0700

    Jenkins test

--- src/ImageList.js
+++ src/ImageList.js
@@ -6,7 +6,7 @@ const ImageList = (props)=>{
             <a key = {image.id} className="ui card" href = {image.urls.regular} target="_blank" style={{height:'300px',backgroundImage:`url('${image.urls.small}')`,backgroundSize:'cover'}}>
                 {/* <a href={image.urls.regular} target="_blank" className="ui image">
                     <div key ={image.id} style={{,height:'100%',width:'100%'}} />
-                </a>  */}
+                </a>   */}
             </a>
             
         )
